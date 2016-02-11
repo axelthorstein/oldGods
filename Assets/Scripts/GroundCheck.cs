4aaff998dd7d4332f69b0fc4e71b2f00de0a3afe
axelthor <axelthorstein@gmail.com>
2016-02-11 17:31:04 -0500

    fixed idle animation, added correct sprite profile when reverse direction

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponentInParent<PlayerController> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		player.grounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		player.grounded = false;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,7 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 50f;
 	public int maxSpeed = 10;
 	public float jumpPower = 150f;
-
-
-	//float moveVelocity;
-
-	// Ground Variables
-
 	public bool grounded = true; 
-
-	//float moveRight = 0;
-	//bool facingLeft = true;
 
 	private Rigidbody2D rd2d;
 	private Animator anime;
@@ -32,7 +23,18 @@ public class PlayerController : MonoBehaviour {
 	
 		anime.SetBool ("Grounded", grounded);
 		anime.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
-	
+
+		if (Input.GetAxis("Horizontal") < -0.1f) {
+
+			transform.localScale = new Vector3 (-1, 1, 1);
+
+		}
+
+		if (Input.GetAxis("Horizontal") > 0.1f) {
+
+			transform.localScale = new Vector3 (1, 1, 1);
+
+		}
 	}
 
 
@@ -41,7 +43,6 @@ public class PlayerController : MonoBehaviour {
 
 
 		float horizontalMove = Input.GetAxis ("Horizontal");
-
 		rd2d.AddForce ((Vector2.right * speed) * horizontalMove);
 
 		if (rd2d.velocity.x > maxSpeed) {
@@ -55,42 +56,5 @@ public class PlayerController : MonoBehaviour {
 			rd2d.velocity = new Vector2 (-maxSpeed, rd2d.velocity.y);
 
 		}
-		// Jumping 
-//		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyUp (KeyCode.UpArrow)) {
-//			if (grounded) {
-//				GetComponent<Rigidbody2D> ().velocity = 
-//					new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
-//			}
-//		}
-
-
-
-
-//		moveVelocity = 0;
-//
-//
-//		if (Input.GetKey (KeyCode.LeftArrow)) {
-//
-//			moveVelocity = -speed;
-//			//transform.forward = new Vector3(1f, 0f, 0f);
-//		}
-//		if (Input.GetKey (KeyCode.RightArrow)) {
-//
-//			moveVelocity = speed;
-//			//transform.forward = new Vector3(-1f, 0f, 0f);
-//		}
-//		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
-//	
-	
-
-		
-
 	}
-	// check if grounded
-//	void OnTriggerEnter2D() {
-//		grounded = true;
-//	}
-//	void OnTriggerExit2D() {
-//		grounded = false;
-//	}
 }

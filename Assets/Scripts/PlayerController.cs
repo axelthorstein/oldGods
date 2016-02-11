e10deaa453eaabfc9470603437885245965cf8e5
axelthor <axelthorstein@gmail.com>
2016-02-10 23:22:18 -0500

    Basic functionality: level, jump, move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Movement
+
+	public float speed;
+	public float jump;
+	float moveVelocity;
+
+	// Ground Variables
+
+	bool grounded = true; 
+	
+	// Update is called once per frame
+	void Update () {
+		// Jumping 
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyUp (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Z) || Input.GetKeyUp (KeyCode.W)) {
+			if (grounded) {
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
+			}
+		}
+
+		moveVelocity = 0;
+
+
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
+
+			moveVelocity = -speed;
+
+		}
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+
+			moveVelocity = speed;
+
+		}
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
+	
+
+		}
+		
+		// check if grounded
+		void OnTriggerEnter2D() {
+		grounded = true;
+		}
+		void OnTriggerExit2D() {
+		grounded = false;
+	}
+}

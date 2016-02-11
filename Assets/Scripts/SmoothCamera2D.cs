85cf821644008ac7dbd9d74d8e92ff1b6a15e527
wiheba kalisa <wihebakalisa@76-9-194-97.beanfield.net>
2016-02-11 09:24:41 -0500

    added minimum and maximum values for camera position

--- Assets/Scripts/SmoothCamera2D.cs
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -8,6 +8,12 @@ public class SmoothCamera2D : MonoBehaviour {
 	private Vector2 velocity;
 	public GameObject player;
 
+	public bool bounds;
+
+	public Vector3 minCameraPos;
+	public Vector3 maxCameraPos;
+
+
 	// Initialize the variable to point to the player
 	void Start() {
 		
@@ -23,6 +29,16 @@ public class SmoothCamera2D : MonoBehaviour {
 		float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
 		transform.position = new Vector3 (posX, posY, transform.position.z);
+
+		if (bounds) {
+
+			transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minCameraPos.x, maxCameraPos.x), 
+				Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y), 
+				Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+		
+		}
+	
+		
 	}
 }
 

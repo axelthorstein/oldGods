4d13f59f10d262559e2d0a90dc8c24895d3860c0
Axel Thor <axelthorstein@gmail.com>
2016-02-13 21:55:47 -0500

    It is now possible to swap characters on the fly, and the fish character has been added. Fox sprite disapears

---
+++ Assets/Scripts/CharacterChanger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterChanger : MonoBehaviour {
+
+	public int selectedCharacter = 1;
+	public string characterName;
+	private GameObject[] characters;
+
+	private void Start()
+	{
+		characters = new GameObject[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			characters[i] = transform.GetChild(i).gameObject;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.X)) {
+			if (characters[0].activeSelf) {
+				characters[1].SetActive (true);
+				characters[1].transform.position = characters[0].transform.position;
+				characters[0].SetActive (false);
+			} else {
+				characters[0].SetActive (true);
+				characters[0].transform.position = characters[1].transform.position;
+				characters[1].SetActive (false);
+			}
+		}
+	}
+}

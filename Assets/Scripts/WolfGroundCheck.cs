a9e801e8617eba6038804aeb50bc7b4e866369e7
axelthor <axelthorstein@gmail.com>
2016-02-19 14:33:38 -0500

    Fixed wolf controller and groundcheck

---
+++ Assets/Scripts/WolfGroundCheck.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfGroundCheck : MonoBehaviour {
+
+	private WolfController player;
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponentInParent<WolfController> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		player.grounded = true;
+	}
+
+	void OnTriggerStay2D(Collider2D col) 
+	{
+		player.grounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		player.grounded = false;
+	}
+}

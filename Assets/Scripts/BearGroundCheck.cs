721ad8148605e83a6451e17946057766d6e7f04f
axelthor <axelthorstein@gmail.com>
2016-02-24 18:21:38 -0500

    added bear sprite

---
+++ Assets/Scripts/BearGroundCheck.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearGroundCheck : MonoBehaviour {
+
+	private BearController player;
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponentInParent<BearController> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		player.grounded = true;
+	}
+
+	void OnTriggerStay2D(Collider2D col) 
+	{
+		player.grounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		player.grounded = false;
+	}
+}
